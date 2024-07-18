b702fc7f286d865ec816eaf7afe7cd560fa72a82
Jonathan <jbmolona413@gmail.com>
2024-07-17 21:57:36 -0500

    Updated data types and variables in Data_Types & Variables.py

---
+++ Python Scripts/Data Types & Variables.py
@@ -0,0 +1,18 @@
+X_int = -10
+X_int2 = -12
+X_int_sum = X_int + X_int2
+
+print(X_int)
+print(X_int2)
+print("Sum of two integer values of x_int and x_int2", X_int_sum)
+print(X_int_sum)
+
+
+
+X_flout = 0.25
+x_print_int_sum = X_flout + X_int
+print(X_flout)
+print(x_print_int_sum)
+
+X_string = "Hello to the world of Data Science"
+print(X_string)

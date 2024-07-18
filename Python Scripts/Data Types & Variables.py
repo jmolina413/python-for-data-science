ab5bff01e9f4f407b80011918a86b46628fa1ce0
Jonathan <jbmolona413@gmail.com>
2024-07-17 23:36:01 -0500

    Updated data types IN lISTS,matrices, and Dictionary.py

--- Python Scripts/Data Types & Variables.py
+++ Python Scripts/Data Types & Variables.py
@@ -1,18 +1,39 @@
-X_int = -10
-X_int2 = -12
-X_int_sum = X_int + X_int2
+# Creating Integer Variables
+x_int = -100
+x_int2 = -12
 
-print(X_int)
-print(X_int2)
-print("Sum of two integer values of x_int and x_int2", X_int_sum)
-print(X_int_sum)
+# Summing Integer Variables
+x_int_sum = x_int + x_int2
 
+# Subtracting Integer Variables
+x_int_sum = x_int + x_int2
+print(x_int)
+print(x_int2)
+print("Sum of two integer values of x_int and x_int2", x_int_sum)
+print(x_int_sum)
 
+# Creating Floating Number Variables
+x_flout = 0.25
 
-X_flout = 0.25
-x_print_int_sum = X_flout + X_int
-print(X_flout)
+# Summing Floating Number and Integer Variables
+x_print_int_sum = x_flout + x_int
+print(x_flout)
 print(x_print_int_sum)
 
-X_string = "Hello to the world of Data Science"
-print(X_string)
+# Creating String Variables
+x_string = "Welcome to the world of Data Science"
+print(x_string)
+
+#Creating Arrays
+x_array = [1.0, 0.25, 0.55, -0.0009]
+x_array2 = [2.33, 0.955, 0.66, -0.33]
+x_array_sum =  x_array + x_array2   # Concatenation of two arrays
+print(x_array)
+print(x_array2)
+print(x_array_sum)
+
+#Creating lists
+x_list1 = [2, "three", 5.0, True]
+x_list2 = [4.0, "4.667", False, True, x_string, 6]
+print(x_list1)
+print(x_list2)

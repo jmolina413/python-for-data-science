5255f91f45d671667024c79af34fdd72a521edd9
Jonathan <jbmolona413@gmail.com>
2024-07-27 15:12:00 -0500

    Your commit message

--- Python Scripts/Project 7. Data Exploration and Preprocessing.py
+++ Python Scripts/Project 7. Data Exploration and Preprocessing.py
@@ -1,8 +1,32 @@
+# Importing necessary libraries
+# pandas is used for data manipulation and analysis
+# sqlite3 is used for interacting with SQLite databases
 import pandas as pd
 import sqlite3
 
-# Loading CVS file with header
-data_cvs = pd.read_cvs("percent_bachelors_degrees_women_usa.csv")
+# Loading CSV file with header
+# pd.read_csv function reads a CSV file into a DataFrame
+# "percent-bachelors-degrees-women-usa.csv" is the name of the CSV file being read
+data_cvs =pd.read_csv("percent-bachelors-degrees-women-usa.csv")
 #print(data_cvs)
 
-print(data_cvs.head(20))
+# Viewing the first 100 rows of the DataFrame
+print(data_cvs.head(100))
+
+# Viewing the last 20 rows of the DataFrame
+print(data_cvs.tail(20))
+
+# Getting a concise summary of the DataFrame
+# This includes the number of non-null entries and data types of each column
+print(data_cvs.info())
+
+# Handling missing values
+
+# Removing rows with any missing values
+print(data_cvs.dropna())
+
+# Replacing missing values with the string "NULL"
+print(data_cvs.fillna("NULL"))
+
+# Removing duplicate rows from the DataFrame
+print(data_cvs.drop_duplicates())
